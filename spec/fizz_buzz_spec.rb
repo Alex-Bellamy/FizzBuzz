f484440adedd77f5694280fb38b624436674b47e
Alex Bellamy <alexlbellamy@gmail.com>
2020-05-26 16:13:15 +0200

    finalised game

--- spec/fizz_buzz_spec.rb
+++ spec/fizz_buzz_spec.rb
@@ -1,12 +1,19 @@
 require './spec/lib/fizz_buzz'
 
-describe 'fizz_buzzzzzz' do
-
-    it 'returns 1 if number is 1' do
-        expect(fizz_buzz(1)). to eq 1
+describe 'fizz_buzz' do
+    it "returns 1 if number is 1" do
+      expect(fizz_buzz(1)).to eq 1 
     end
-
-    it 'returns fizz if number is divisible by 3' do
-        expect(fizz_buzz(3)).to eq ‘Fizz’
+  
+    it "returns 'fizz' if number is divisible by 3" do
+      expect(fizz_buzz(9)).to eq 'fizz'
     end
-end
+  
+    it "returns 'buzz' if number is divisible by 5" do
+      expect(fizz_buzz(35)).to eq 'buzz'
+    end
+  
+    it "returns 'fizz buzz' if number is divisible by 15" do
+      expect(fizz_buzz(45)).to eq 'fizz buzz'
+    end
+  end
--- spec/lib/fizz_buzz.rb
+++ spec/lib/fizz_buzz.rb
@@ -1,8 +1,15 @@
-def fizz_buzz(numbers)
-    if number % 3 == 0
-        'fizz'
-    else
-        number
-    end
-    
+def fizz_buzz(number)
+  if has_zero_remainder?(number, 15)
+    'fizz buzz'
+  elsif has_zero_remainder?(number, 5)
+    'buzz'
+  elsif has_zero_remainder?(number, 3)
+    'fizz'
+  else
+    number
+  end
+end
+
+def has_zero_remainder?(number, divider)
+  number % divider == 0
 end
